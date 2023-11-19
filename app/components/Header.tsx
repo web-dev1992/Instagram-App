8a64dd769162b0b3194f6cd407ebc7096b86fae6
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-11-19 16:25:09 +0330

    add search form to the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,34 +1,43 @@
 import { FC } from "react";
 import Image from "next/image";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 interface IHeaderProps {}
 
 export const Header: FC<IHeaderProps> = (props) => {
   return (
-    <div>
+    <div className="flex items-center justify-between max-w-6xl m-auto">
       {/* left part */}
-      <div className="flex items-center justify-between max-w-6xl m-auto">
-        <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-          <Image
-            src="/images/Instagram_logo.png"
-            width={100}
-            height={200}
-            alt="Instagram Logo"
-            className="object-contain"
-          />
-        </div>
-        <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
-          <Image
-            src="/images/instagram.jpeg"
-            width={100}
-            height={200}
-            alt="Instagram Logo"
-            className="object-contain"
-          />
-        </div>
-        <h1>right side</h1>
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          src="/images/Instagram_logo.png"
+          width={100}
+          height={200}
+          alt="Instagram Logo"
+          className="object-contain"
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
+        <Image
+          src="/images/instagram.jpeg"
+          width={100}
+          height={200}
+          alt="Instagram Logo"
+          className="object-contain"
+        />
       </div>
       {/* middle part */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <MagnifyingGlassIcon className="h-5 text-gray-500" />
+        </div>
+        <input
+          type="text"
+          placeholder="Search "
+          className="bg-gray-50 pl-10  border-gray-500 text-sm focus:ring-black focus:border-black rounded-md "
+        />
+      </div>
       {/* right part */}
+      <h1>Right side</h1>
     </div>
   );
 };
