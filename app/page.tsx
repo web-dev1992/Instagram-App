a46db8a8521db2f326a90d4561f74fae996cb457
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-11-19 13:00:14 +0330

    initialize the project

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Fragment } from "react";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Head>
+        <title>Instagram App</title>
+        <meta name="description" content="" />
+        <link rel="icon" href="./favicon/ico" />
+      </Head>
+
+      {/* header */}
+      {/* feed */}
+      {/* modal */}
+    </Fragment>
+  );
+}
