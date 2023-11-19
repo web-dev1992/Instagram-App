a9dcb4ef9308bc540b78bfad4fe8cc2223f09c85
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-11-19 18:09:22 +0330

    add profile image  and menu to the Header component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 import Image from "next/image";
-import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import {
+  MagnifyingGlassIcon,
+  PlusCircleIcon,
+} from "@heroicons/react/24/outline";
+import { HomeIcon } from "@heroicons/react/24/solid";
+
 interface IHeaderProps {}
 
 export const Header: FC<IHeaderProps> = (props) => {
   return (
-    <div className="flex items-center justify-between max-w-6xl m-auto">
+    <div className="flex content-center items-center justify-between max-w-6xl mx-4 xl:m-auto ">
       {/* left part */}
-      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid content-center">
         <Image
           src="/images/Instagram_logo.png"
           width={100}
@@ -16,7 +21,7 @@ export const Header: FC<IHeaderProps> = (props) => {
           className="object-contain"
         />
       </div>
-      <div className="cursor-pointer h-24 w-10 relative  lg:hidden">
+      <div className="cursor-pointer h-24 w-10 relative  lg:hidden flex items-center contect-center">
         <Image
           src="/images/instagram.jpeg"
           width={100}
@@ -37,7 +42,11 @@ export const Header: FC<IHeaderProps> = (props) => {
         />
       </div>
       {/* right part */}
-      <h1>Right side</h1>
+      <div className="flex space-x-4 items-center ">
+        <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+        <img src="/images/taraneh.jpg" alt="user-image" className="h-10 rounded-full cursor-pointer " />
+      </div>
     </div>
   );
 };
