cb8f4177a811d718c25aeb46efff33fbcaaf2b2a
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-11-20 14:00:16 +0330

    make the feed section responsive

--- app/components/Feed.tsx
+++ app/components/Feed.tsx
@@ -5,15 +5,15 @@ interface IFeedProps {}
 
 export const Feed: FC<IFeedProps> = (props) => {
   return (
-    <main>
-      <section>
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto ">
+      <section className="col-span-2">
         {/* Stories section */}
         <Stories />
 
         {/* Posts section */}
         <Posts/>
       </section>
-      <section>
+      <section className="md:col-span-1 hidden md:inline-grid">
         {/* Mini Profile */}
 
         {/* Suggestions */}
