17d869d1541e41792d55436992ead2d301d1cbd8
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-12-04 15:05:37 +0330

    add authorization key to google provider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,9 +6,16 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    //   authorization:
-    //     "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
-     }),
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+      //   authorization:
+      //     "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+    }),
     // ...add more providers here
   ],
   secret: process.env.NEXTAUTH_SECRET as string,
