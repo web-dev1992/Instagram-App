a895da07f3ff6781465097b4691fea4255080b08
Taraneh Mohebi <taraneh.m5257@gmail.com>
2023-12-07 11:35:42 +0330

    change story plusIcon

--- components/Story.tsx
+++ components/Story.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import Image from "next/image";
-import { PlusCircleIcon } from "@heroicons/react/24/solid";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 interface IStoryProps {
   img: string;
@@ -19,7 +19,7 @@ export const Story: FC<IStoryProps> = ({ img, username, isUser = false }) => {
         className="h-12 w-12 rounded-full border-2 border-red-500 p-[1.5px] group-hover:scale-110 transition-transform duration-200 ease-out"
       />
       {isUser && (
-        <PlusCircleIcon className="h-6 absolute top-4 left-4 text-white border" />
+        <PlusIcon className="h-6 w-6 absolute top-[14px] left-[14px] text-black" />
       )}
       <p className="text-xs w-14 truncate">{username}</p>
     </div>
